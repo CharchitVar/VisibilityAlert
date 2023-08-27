a79388d4780f1a5e40a402d47818ebd55d79e477
charchit <charchit.v05@gmail.com>
2023-08-27 11:37:29 +0530

    commit changes for initial compplete code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { computeDestinationPoint } from 'geolib';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,63 +13,106 @@ export class AppComponent {
   currentLat: any;
   currentLng: any;
   heading: any;
+  newCoords: any;
+  visibility: number =0;
+  areaName: string ='';
+  distance: number =10;
+  firstResponseCame: boolean = false;
+  loading = false;
+  isError = false;
+  movingDirection: string ='';
   constructor(private http: HttpClient){
-
   }
-  calculateBearingAngle(): void {
-    navigator.geolocation.getCurrentPosition((position) => {
-      // Get device orientation data
-      setInterval(()=>{window.addEventListener('deviceorientation', (event) => {
-        console.log(event);
-         this.heading = event.alpha; // Device heading in degrees (0 to 360)
+  intervalListener: any;
+  errorMessage: string ='';
+  calculateStart(){
 
-        // this.heading = 45;
-        // Calculate bearing angle
+      this.loading = true;
+      this.intervalListener = setInterval(() => {
+       if(this.distance == 0){
+         this.distance = 10;
+       }
+       this.calculateVisibilty();
+       this.firstResponseCame = true;
+       this.loading = false;
+     }, 5000); // 1 minute in milliseconds
+    
+   
+  }
+  onDistanceChange(ev: any){
+    this.firstResponseCame = false;
+    this.loading = true;
+    this.calculateStart();
+  }
+  calculateVisibilty(): void {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const orientationHandler = (event: any) => {
+        console.log(event);
+        this.heading = event.alpha; // Device heading in degrees (0 to 360)
+        this.movingDirection = this.getDirectionFromAlpha(this.heading);
+
         this.currentLat = position.coords.latitude;
         this.currentLng = position.coords.longitude;
-        // let address="Chennai";
-        // let url="https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURIComponent(address)+".json?access_token=pk.eyJ1IjoiY2hhcmNoaXR2MDUiLCJhIjoiY2p5d29ocmRlMHppNDNjcnIwZ3I2YzBibCJ9.8yxqlYvIibh6_iBQCKuAKQ&limit=1";
-        // this.getLatandLang(url);
 
-        // Target location (replace with your desired location)
-        const targetLat = 13.08;
-        const targetLng = 80.27;
+        const distance = Number(this.distance) * 1000;
 
-        // Calculate latitudinal and longitudinal difference
-        const latDiff = targetLat - this.currentLat;
-        const lngDiff = targetLng - this.currentLng;
+        const newCoords = computeDestinationPoint(
+          { latitude: this.currentLat, longitude: this.currentLng },
+          distance,
+          this.heading
+        );
+        this.newCoords = newCoords;
+        this.getVisibility(this.newCoords);
 
-        // Calculate bearing angle using trigonometry
-        const bearing = (Math.atan2(lngDiff, latDiff) * 180) / Math.PI;
+        window.removeEventListener('deviceorientation', orientationHandler);
+      };
 
-        const distance = 2186000; // Distance in meters (10 km)
-
-    const newCoords = computeDestinationPoint(
-      { latitude: this.currentLat, longitude: this.currentLng },
-      distance,
-      bearing
-    );
-
-        console.log("Device Heading:", this.heading);
-        console.log("Bearing Angle:", bearing);
-
-    const newLat = newCoords.latitude;
-    const newLng = newCoords.longitude;
-
-    console.log("Current Location:", this.currentLat, this.currentLng);
-    console.log("New Location 10 km away:", newLat, newLng);
-
-
-        // Now you can use the bearing angle as needed
-      });},1000)
-
+      window.addEventListener('deviceorientation', orientationHandler);
+      this.firstResponseCame = true;
+      this.isError = false;
+    },(error: GeolocationPositionError)=>{
+      if(error){
+        this.firstResponseCame = false;
+        this.isError = true;
+        this.errorMessage = 'Error in Getting your Location. Please check the location access of your Browser ';
+      }
+    
     });
-
   }
 
-  getLatandLang(url: any){
-    this.http.get(url).subscribe((response)=>{
+  getDirectionFromAlpha(alpha: number): string {
+    if (alpha >= 0 && alpha < 45) {
+      return 'North';
+    } else if (alpha >= 45 && alpha < 135) {
+      return 'East';
+    } else if (alpha >= 135 && alpha < 225) {
+      return 'South';
+    } else if (alpha >= 225 && alpha < 315) {
+      return 'West';
+    } else {
+      return 'North';
+    }
+  }
+
+  getVisibility(newCoords: any){
+    
+   const url ='https://api.openweathermap.org/data/2.5/weather?lat='+newCoords.latitude+'&lon='+newCoords.longitude+'&appid=78c692fdd6147f943a2ca9277a3249bf';
+    this.http.get(url).subscribe((response: any)=>{
       console.log(response);
+      this.firstResponseCame = true;
+      this.visibility = response.visibility/1000;
+      this.areaName = response.name;
+      window.removeEventListener('deviceorientation',()=>{
+
+      });
     })
   }
+  clickHomePage(){
+    this.firstResponseCame = false;
+    this.distance =10;
+    window.clearInterval(this.intervalListener);
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -13,8 +18,9 @@ import {HttpClientModule} from '@angular/common/http'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
+    HttpClientModule, FormsModule, BrowserAnimationsModule,
+    MatInputModule, MatButtonModule, MatFormFieldModule
+  ],  
   providers: [],
   bootstrap: [AppComponent]
 })
